2fff5f99e053eb913aae7c790454a3cc6bd20206
Rajat Jangid <68rajat68@gmail.com>
2025-02-11 19:45:49 +0530

    ssh test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <h2>Hello NoteApp</h2>
+      <h2>SSH Key Added</h2>
     </>
   );
 }
