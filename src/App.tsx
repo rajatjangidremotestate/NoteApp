ff642973839510489bbcb77d43075ea48247d9c6
Rajat Jangid <68rajat68@gmail.com>
2025-02-12 11:19:00 +0530

    Clean Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h2>Hello NoteApp</h2>
-      <h2>Dev Branch</h2>
+      <h1>Note App</h1>
     </>
   );
 }
